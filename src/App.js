b3e9775b19d9136fe0245e8ad0348b975a512537
iamwwc <qaq1362211689@gmail.com>
2020-04-16 20:27:37 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import RootRouter from './common/index'
 import reducers from './reducers'
 import { Provider } from 'react-redux';
+
 import configureStore from "./common/configureStore";
+
+import './App.scss';
+import 'antd/dist/antd.css';
+
 const store = configureStore(reducers)
 
 export class App extends Component {
--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
 import { connect } from 'react-redux'
-import { Home } from '../components/routes'
-const Router = HashRouter
+import { Home,Staff } from '../routes'
+import Nav from '../features/nav'
+const Router = BrowserRouter
 
 export default connect(state => {
   return {
@@ -14,9 +15,13 @@ export default connect(state => {
       return (
         <React.Fragment>
           <Router>
-            <Switch>
-              <Route exact path="/" component={Home} ></Route>
-            </Switch>
+            <Nav></Nav>
+            <div className="display-content">
+              <Switch>
+                <Route exact path="/" component={Home} ></Route>
+                <Route path="/user/staff" component={Staff}></Route>
+              </Switch>
+            </div>
           </Router>
         </React.Fragment>
       )
